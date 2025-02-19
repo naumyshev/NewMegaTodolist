7f95e59ade3d0405e2f0b1ff3dc1526de1a9c47e
Aleksei Naumyshev <naumyshev@yandex.ru>
2025-02-19 14:30:12 +0300

    lesson 9: refactored tasks-reducer.ts

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -11,7 +11,7 @@ export const createTaskAC = createAction<{title: string, todolistId: string}>('t
 
 export const changeTaskStatusAC = createAction<{todolistId: string, taskId: string, isDone: boolean}>('tasks/changeTaskStatus')
 
-export const changeTaskTitleAC = createAction<{todolistId: string, taskId: string, title: string}>('tasks/changeTaskStatus')
+export const changeTaskTitleAC = createAction<{todolistId: string, taskId: string, title: string}>('tasks/changeTaskTitle')
 
 
 
@@ -32,6 +32,20 @@ export const tasksReducer = createReducer(initialState, builder => {
         .addCase(createTaskAC, (state, action) => {
             const newTask = {id: nanoid(), title: action.payload.title, isDone: false}
             state[action.payload.todolistId].unshift(newTask)
+        })
+        .addCase(changeTaskStatusAC, (state, action) => {
+            const tasks = state[action.payload.todolistId]
+            const task = tasks.find(task => task.id === action.payload.taskId)
+            if (task) {
+                task.isDone = action.payload.isDone
+            }
+        })
+        .addCase(changeTaskTitleAC, (state, action) => {
+            const tasks = state[action.payload.todolistId]
+            const task = tasks.find(task => task.id === action.payload.taskId)
+            if (task) {
+                task.title = action.payload.title
+            }
         })
 })
 
