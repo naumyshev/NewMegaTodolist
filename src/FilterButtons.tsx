2882cc52db560714c153010ce53dcf393f43d4f1
Aleksei Naumyshev <naumyshev@yandex.ru>
2025-03-03 13:47:07 +0300

    lesson 10: added FilterButtons.tsx

---
+++ src/FilterButtons.tsx
@@ -0,0 +1,40 @@
+import {containerSx} from "@/TodolistItem.styles.ts";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import {changeTodolistFilterAC, Todolist} from "@/model/todolists-reducer.ts";
+import {FilterValues} from "@/app/App.tsx";
+import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
+
+type Props = {
+    todolist: Todolist
+}
+
+export const FilterButtons = ({todolist}: Props) => {
+    const {id, filter} = todolist
+
+    const dispatch = useAppDispatch()
+
+    const changeFilter = (filter: FilterValues) => {
+        dispatch(changeTodolistFilterAC({id, filter}))
+    }
+
+    return (
+        <Box sx={containerSx}>
+            <Button variant={filter === 'all' ? 'outlined' : 'text'}
+                    color={'inherit'}
+                    onClick={() => changeFilter('all')}>
+                All
+            </Button>
+            <Button variant={filter === 'active' ? 'outlined' : 'text'}
+                    color={'primary'}
+                    onClick={() => changeFilter('active')}>
+                Active
+            </Button>
+            <Button variant={filter === 'completed' ? 'outlined' : 'text'}
+                    color={'secondary'}
+                    onClick={() => changeFilter('completed')}>
+                Completed
+            </Button>
+        </Box>
+    );
+};
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,14 +1,10 @@
-import {FilterValues} from "./app/App.tsx";
 import {CreateItemForm} from "./CreateItemForm.tsx";
-import Button from "@mui/material/Button";
-import Box from '@mui/material/Box'
-import {containerSx} from './TodolistItem.styles'
-import {changeTodolistFilterAC, Todolist} from "@/model/todolists-reducer.ts";
+import {Todolist} from "@/model/todolists-reducer.ts";
 import {createTaskAC} from "@/model/tasks-reducer.ts";
 import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
 import {TodolistTitle} from "@/TodolistTitle.tsx";
 import {Tasks} from "@/Tasks.tsx";
-
+import {FilterButtons} from "@/FilterButtons.tsx";
 
 type Props = {
     todolist: Todolist
@@ -17,14 +13,10 @@ type Props = {
 export const TodolistItem = (props: Props) => {
 
     const {
-        todolist: {id, filter}
+        todolist: {id}
     } = props
 
     const dispatch = useAppDispatch()
-
-    const changeFilter = (filter: FilterValues) => {
-        dispatch(changeTodolistFilterAC({id, filter}))
-    }
 
     const createTask = (title: string) => {
         dispatch(createTaskAC({todolistId: id, title}))
@@ -33,28 +25,9 @@ export const TodolistItem = (props: Props) => {
     return (
         <div>
             <TodolistTitle todolist={props.todolist} />
-
             <CreateItemForm onCreateItem={createTask}/>
-
             <Tasks todolist={props.todolist} />
-
-            <Box sx={containerSx}>
-                <Button variant={filter === 'all' ? 'outlined' : 'text'}
-                        color={'inherit'}
-                        onClick={() => changeFilter('all')}>
-                    All
-                </Button>
-                <Button variant={filter === 'active' ? 'outlined' : 'text'}
-                        color={'primary'}
-                        onClick={() => changeFilter('active')}>
-                    Active
-                </Button>
-                <Button variant={filter === 'completed' ? 'outlined' : 'text'}
-                        color={'secondary'}
-                        onClick={() => changeFilter('completed')}>
-                    Completed
-                </Button>
-            </Box>
+            <FilterButtons todolist={props.todolist} />
         </div>
     );
 };
